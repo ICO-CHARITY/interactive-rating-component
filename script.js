23bb44595805ac6657842b6c17f2ccce0f3d6518
ICO-CHARITY <charity.ilouga@gmail.com>
2025-10-27 09:32:15 +0100

    fixed layout issues

--- script.js
+++ script.js
@@ -16,22 +16,40 @@ let selectedValue = 0;
 
 //when a circle is clicked
 circle.forEach(function (button) {
-  button.addEventListener('click', function () {
-    const value = parseInt(this.getAttribute('data-value'));
+  button.addEventListener('click', function (e) {
+    //no e.preventDefault(); because is a button check the html
+
+    // const value = parseInt(this.getAttribute('data-value'));
     console.log(selectedValue);
 
     if (selectedValue < 5) {
       selectedValue++;
     } else {
-      alert('you exceeded the maximum limit.');
+      selectedValue--;
     }
   });
 });
 
 //when submit button is clicked
-submitBtn.addEventListener('click', function () {
-  container2.classList.remove('hidden');
-  //display selected value in result div
+submitBtn.addEventListener('click', function (e) {
+  e.preventDefault(); //prevent reload
+  container2.classList.remove('hidden'); //show container2 card
 
-  result.textContent = `You selected ${selectedValue} out of ${circle.length}`;
+  result.textContent = `You selected ${selectedValue} out of ${circle.length}`; //display selected value in result div
+});
+
+//dark mode toggle
+const star = document.querySelector('.star');
+const body = document.body;
+
+star.addEventListener('click', function () {
+  console.log('hello dark mode');
+  const isDark = body.dataset.theme === 'dark';
+  body.dataset.theme = isDark ? 'light' : 'dark';
+  localStorage.setItem('theme', isDark ? 'light' : 'dark');
+});
+
+window.addEventListener('DOMContentLoaded', function () {
+  const saveTheme = this.localStorage.getItem('theme') || 'light';
+  body.dataset.theme = saveTheme;
 });
